702f620bc8705c6e93f2532a19688741e855f847
Luka Bacic <luka.bacic278@gmail.com>
2021-01-26 19:53:45 +1100

    Import style to Layout instead into the page

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'scss/style.scss';
 
 export default function Layout({ children }) {
   return (
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import GoogleMap from 'components/GoogleMap';
-import 'scss/style.scss';
 
 const IndexPage = () => {
   return <GoogleMap />;
