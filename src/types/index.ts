ebdb600e4fe2a64ef4de5bc4e87f35e0ecfc9604
Luka Bacic <luka.bacic278@gmail.com>
2021-02-10 15:32:28 +1100

    Add types for daily weather response

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,4 +2,44 @@ export interface UvInfo {
   value: number;
   description: string;
   longDescription: string;
+  cssClass: string;
+}
+
+export interface Weather {
+  description: string;
+  icon: string;
+  id: number;
+  main: string;
+}
+
+export interface DailyResponse {
+  dt: number;
+  sunrise: number;
+  sunset: number;
+  temp: {
+    morn: number;
+    day: number;
+    eve: number;
+    night: number;
+    min: number;
+    max: number;
+  };
+  feels_like: {
+    morn: number;
+    day: number;
+    eve: number;
+    night: number;
+  };
+  pressure: number;
+  humidity: number;
+  dew_point: number;
+  wind_speed: number;
+  wind_gust: number;
+  wind_deg: number;
+  clouds: number;
+  uvi: number;
+  pop: number;
+  rain: number;
+  snow: number;
+  weather: Weather[];
 }
