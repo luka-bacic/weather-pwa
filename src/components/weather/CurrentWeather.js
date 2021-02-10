6a20765e4d37cda01be635f8d333b391e3163020
Luka Bacic <luka.bacic278@gmail.com>
2021-02-10 15:35:17 +1100

    Rename class

--- src/components/weather/CurrentWeather.js
+++ src/components/weather/CurrentWeather.js
@@ -2,21 +2,19 @@ import React from 'react';
 import { hasProp } from 'functions/hasProp';
 
 const CurrentWeather = ({ data }) => {
-  console.log(data);
+  // console.log('now', data);
   return (
-    <section className="currently">
+    <section className="current">
       {hasProp(data, 'temp') && (
         <p className="currently__temperature">{data.temp}&deg;</p>
       )}
 
       {hasProp(data, 'feels_like') && (
-        <p className="currently__feels-like">
-          Feels like {data.feels_like}&deg;
-        </p>
+        <p className="current__feels-like">Feels like {data.feels_like}&deg;</p>
       )}
 
-      {hasProp(data.weather, 'description') && (
-        <p className="currently__description">{data.weather.description}</p>
+      {hasProp(data.weather[0], 'description') && (
+        <p className="current__description">{data.weather[0].description}</p>
       )}
     </section>
   );
