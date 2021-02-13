7092117dfbe69e2049b54037fba666cc4f5d2407
Luka Bacic <luka.bacic278@gmail.com>
2021-02-13 19:37:28 +1100

    Render only if data is valid

--- src/components/weather/reusable/UvIndex.tsx
+++ src/components/weather/reusable/UvIndex.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Tooltip from 'components/Tooltip';
-
+import { UvInfo } from 'types';
 import uvScale from 'functions/uvScale';
 
 type Props = {
@@ -8,17 +8,27 @@ type Props = {
 };
 
 const UvIndex = ({ uv }: Props) => {
-  const uvData = uvScale(uv);
+  const [uvData, setUvData] = useState<UvInfo | undefined>(undefined);
 
-  return (
-    <span className="uv-info">
-      <span className={`uv-info__circle ${uvData.cssClass}`}>
-        <span className="sr-only">uv index</span>
+  useEffect(() => {
+    if (typeof uv !== 'undefined' && uv >= 0) {
+      setUvData(uvScale(uv));
+    }
+  }, [uv]);
+
+  if (typeof uvData !== 'undefined') {
+    return (
+      <span className="uv-info">
+        <span className={`uv-info__circle ${uvData.cssClass}`}>
+          <span className="sr-only">uv index</span>
+        </span>
+        {uvData.value}&nbsp;({uvData.description})
+        <Tooltip text={uvData.longDescription} />
       </span>
-      {uvData.value}&nbsp;({uvData.description})
-      <Tooltip text={uvData.longDescription} />
-    </span>
-  );
+    );
+  } else {
+    return null;
+  }
 };
 
-export default UvIndex;
+export default React.memo(UvIndex);
