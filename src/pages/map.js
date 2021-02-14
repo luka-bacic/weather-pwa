1635332b6c4d2bef14eeaa4e3d6fca30e873f349
Luka Bacic <luka.bacic278@gmail.com>
2021-02-14 18:15:54 +1100

    Fix SSR issues with map

--- src/pages/map.js
+++ src/pages/map.js
@@ -4,7 +4,12 @@ import WorldMap from '../components/WorldMap';
 const pickLocation = () => {
   return (
     <div>
-      <WorldMap />
+      {/* Render the map only in client - react-leaflet doesn't support SSR */}
+      {typeof window !== 'undefined' ? (
+        <WorldMap />
+      ) : (
+        'Pick a location from the map to see the weather'
+      )}
     </div>
   );
 };
