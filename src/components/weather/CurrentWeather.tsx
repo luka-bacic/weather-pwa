ec870f17c8fa93a313b37700ae7f218824bdf6bc
Luka Bacic <luka.bacic278@gmail.com>
2021-02-20 19:07:24 +1100

    Make component logic more readable; Add icon

--- src/components/weather/CurrentWeather.tsx
+++ src/components/weather/CurrentWeather.tsx
@@ -1,23 +1,52 @@
-import React from 'react';
-import { hasProp, round } from 'functions';
-import { CurrentResponse } from 'types';
+import React, { useState, useEffect } from 'react';
+import { hasProp, round, getIconInfo } from 'functions';
+import { CurrentResponse, IconData } from 'types';
 
 type Props = {
   data: CurrentResponse;
 };
 
 const CurrentWeather = ({ data }: Props) => {
-  // console.log('now', data);
+  const [temp, setTemp] = useState<number | null>(null);
+  const [feelsLike, setFeelsLike] = useState<number | null>(null);
+  const [iconData, setIconData] = useState<IconData | null>(null);
+
+  useEffect(() => {
+    if (hasProp(data, 'temp')) {
+      setTemp(round(data.temp, 1));
+    }
+
+    if (hasProp(data, 'feels_like')) {
+      setFeelsLike(round(data.feels_like, 1));
+    }
+
+    if (hasProp(data, 'weather') && data.weather.length > 0) {
+      if (hasProp(data.weather[0], 'description')) {
+        setIconData(getIconInfo(data.weather[0], '2x'));
+      }
+    }
+  }, [data]);
+
   return (
     <section className="current">
-      {hasProp(data, 'temp') && (
-        <div className="current__temperature">{round(data.temp, 1)}&deg;</div>
-      )}
+      <div>
+        {temp !== null && (
+          <div className="current__temperature">{temp}&deg;</div>
+        )}
 
-      {hasProp(data, 'feels_like') && (
-        <div className="current__feels-like">
-          Feels like <strong>{round(data.feels_like, 1)}&deg;</strong>
-        </div>
+        {feelsLike !== null && (
+          <div className="current__feels-like">
+            Feels like <strong>{feelsLike}&deg;</strong>
+          </div>
+        )}
+      </div>
+
+      {iconData !== null && (
+        <img
+          className="current__icon"
+          src={iconData.url}
+          alt={iconData.description}
+        />
       )}
     </section>
   );
