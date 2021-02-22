494ed6e08251602f2da44ff6b686fc33462f9261
Luka Bacic <luka.bacic278@gmail.com>
2021-02-22 13:39:53 +1100

    Add prop to show most important weather info in the weather expand button

--- src/components/weather/reusable/DailyBlock.tsx
+++ src/components/weather/reusable/DailyBlock.tsx
@@ -1,28 +1,30 @@
 import React, { useState, useEffect } from 'react';
-import { hasProp, round } from 'functions';
+import { getIconInfo, hasProp, round } from 'functions';
 import WindInfo from 'components/weather/reusable/WindInfo';
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
-import { DailyResponse } from 'types';
+import { DailyResponse, IconData } from 'types';
 import UvIndex from './UvIndex';
+import classNames from 'classnames';
 
 type Props = {
   data: DailyResponse;
   timezoneOffset: number;
-  hideTempAndPrecip?: boolean;
+  single?: boolean;
   title?: string;
 };
 
-const DailyBlock = ({
-  data,
-  timezoneOffset,
-  hideTempAndPrecip,
-  title,
-}: Props) => {
+const DailyBlock = ({ data, timezoneOffset, single, title }: Props) => {
   dayjs.extend(utc);
 
   const [sunrise, setSunrise] = useState('');
   const [sunset, setSunset] = useState('');
+  const [minTemp, setMinTemp] = useState<number | null>(null);
+  const [maxTemp, setMaxTemp] = useState<number | null>(null);
+  const [precip, setPrecip] = useState<number | null>(null);
+  const [rain, setRain] = useState<number | null>(null);
+  const [snow, setSnow] = useState<number | null>(null);
+  const [iconData, setIconData] = useState<IconData | null>(null);
   const [renderPrecip, setRenderPrecip] = useState(false);
   const [renderOther, setRenderOther] = useState(false);
   const [renderWind, setRenderWind] = useState(false);
@@ -32,10 +34,24 @@ const DailyBlock = ({
 
   useEffect(() => {
     if (typeof data !== 'undefined') {
-      if (!hideTempAndPrecip) {
+      if (!single) {
+        // Get weather icon data
+        if (hasProp(data.weather[0], 'icon')) {
+          // Get required icon data
+          setIconData(getIconInfo(data.weather[0]));
+        }
+
         // Should the temperature section be rendered
         if (hasProp(data.temp, 'max') || hasProp(data.temp, 'min')) {
           setRenderTemp(true);
+
+          if (hasProp(data.temp, 'max')) {
+            setMaxTemp(round(data.temp.max, 0));
+          }
+
+          if (hasProp(data.temp, 'min')) {
+            setMinTemp(round(data.temp.min, 0));
+          }
         }
 
         // Should the precipitation section be rendered
@@ -45,6 +61,16 @@ const DailyBlock = ({
           hasProp(data, 'snow')
         ) {
           setRenderPrecip(true);
+
+          if (hasProp(data, 'pop')) {
+            setPrecip(round(data.pop * 100, 0));
+          }
+          if (hasProp(data, 'rain') && typeof data.rain !== 'undefined') {
+            setRain(round(data.rain, 0));
+          }
+          if (hasProp(data, 'snow') && typeof data.snow !== 'undefined') {
+            setSnow(round(data.snow, 0));
+          }
         }
       }
 
@@ -125,106 +151,130 @@ const DailyBlock = ({
     }
   }, [data]);
 
+  const precipBackgroundClasses = classNames({
+    'day__title-precip-background': true,
+    'day--no-rain': precip !== null && precip <= 0.1 ? true : false,
+  });
+
   return (
-    <section className="day">
-      <details>
-        <summary>{btnTitle}</summary>
+    <details className="day">
+      <summary className="day__button">
+        <span className="day__title-text">{btnTitle}</span>
+        {!single && (
+          <div className="day__title-info">
+            {iconData !== null && (
+              <img
+                className="day__title-icon"
+                src={iconData.url}
+                alt={iconData.description}
+              />
+            )}
 
-        {!hideTempAndPrecip && (
-          <>
-            {renderTemp && (
-              <div className="day__temperature">
-                <h4>Temperature</h4>
-                <div>
-                  {hasProp(data.temp, 'max') && (
-                    <p className="day__max">
-                      {round(data.temp.max, 1)}&deg; max
-                    </p>
-                  )}
-                  {hasProp(data.temp, 'min') && (
-                    <p className="day__min">
-                      {round(data.temp.min, 1)}&deg; min
-                    </p>
-                  )}
-                </div>
+            {minTemp !== null && (
+              <span className="day__title-min">{minTemp}&deg;</span>
+            )}
+
+            {maxTemp !== null && (
+              <span className="day__title-max">
+                <strong>{maxTemp}</strong>&deg;
+              </span>
+            )}
+
+            {precip !== null && (
+              <span className="day__title-precip">
+                <div
+                  className={precipBackgroundClasses}
+                  style={{ opacity: `${precip}%` }}
+                ></div>
+                <div className="day__title-precip-text">{precip}%</div>
+              </span>
+            )}
+          </div>
+        )}
+      </summary>
+
+      {!single && (
+        <>
+          {renderTemp && (
+            <div className="day__temperature">
+              <h4>Temperature</h4>
+              <div>
+                {minTemp !== null && <div>{minTemp}&deg; min</div>}
+                {maxTemp !== null && <div>{maxTemp}&deg; max</div>}
               </div>
-            )}
+            </div>
+          )}
 
-            {renderPrecip && (
-              <div className="day__precip">
-                <h4>Precipitation</h4>
-                {hasProp(data, 'pop') && (
-                  <p className="day__precip-chance">
-                    {data.pop * 100}% chance of any rain
-                  </p>
-                )}
-                {hasProp(data, 'rain') && (
-                  <p className="day__rain">{data.rain}mm of rain</p>
-                )}
-                {hasProp(data, 'snow') && (
-                  <p className="day__snow">{data.snow}mm of snow</p>
-                )}
-              </div>
-            )}
-          </>
-        )}
+          {renderPrecip && (
+            <div className="day__precip">
+              <h4>Precipitation</h4>
+              {precip !== null && (
+                <p className="day__precip-chance">
+                  {precip}% chance of any rain
+                </p>
+              )}
+              {rain !== null && <p className="day__rain">{rain}mm of rain</p>}
+              {snow !== null && <p className="day__snow">{snow}mm of snow</p>}
+            </div>
+          )}
+        </>
+      )}
 
-        {renderSunUv && (
-          <div className="day__sun-uv">
-            <h4>Sun and UV</h4>
-            {sunrise && (
-              <p className="day__sunrise">
-                <strong>{sunrise}</strong> sunrise
-              </p>
-            )}
+      {renderSunUv && (
+        <div className="day__sun-uv">
+          <h4>Sun and UV</h4>
+          {sunrise && (
+            <p className="day__sunrise">
+              <strong>{sunrise}</strong> sunrise
+            </p>
+          )}
 
-            {sunset && (
-              <p className="day__sunset">
-                <strong>{sunset}</strong> sunset
-              </p>
-            )}
+          {sunset && (
+            <p className="day__sunset">
+              <strong>{sunset}</strong> sunset
+            </p>
+          )}
 
-            {hasProp(data, 'uvi') && (
-              <p className="day__uv-index">
-                <UvIndex uv={data.uvi} />
-              </p>
-            )}
-          </div>
-        )}
+          {hasProp(data, 'uvi') && (
+            <p className="day__uv-index">
+              <UvIndex uv={data.uvi} />
+            </p>
+          )}
+        </div>
+      )}
 
-        {renderWind && (
-          <div className="day__wind">
-            <h4>Wind</h4>
-            <WindInfo degrees={data.wind_deg} speed={data.wind_speed} />
+      {renderWind && (
+        <div className="day__wind">
+          <h4>Wind</h4>
+          <WindInfo degrees={data.wind_deg} speed={data.wind_speed} />
 
-            {hasProp(data, 'wind_gust') && (
-              <p className="day__wind-speed">{data.wind_gust} m/s wind gust</p>
-            )}
-          </div>
-        )}
+          {hasProp(data, 'wind_gust') && (
+            <p className="day__wind-speed">{data.wind_gust} m/s wind gust</p>
+          )}
+        </div>
+      )}
 
-        {renderOther && (
-          <div className="day__other">
-            <h4>Other</h4>
-            {hasProp(data, 'clouds') && (
-              <p className="day__clouds">{data.clouds}% cloud cover</p>
-            )}
+      {renderOther && (
+        <div className="day__other">
+          <h4>Other</h4>
+          {hasProp(data, 'clouds') && (
+            <p className="day__clouds">{data.clouds}% cloud cover</p>
+          )}
 
-            {hasProp(data, 'dew_point') && (
-              <p className="day__dew-point">
-                {round(data.dew_point, 1)}&deg; dew point
-              </p>
-            )}
-            {hasProp(data, 'humidity') && (
-              <p className="day__humidity">{data.humidity}% humidity</p>
-            )}
-            {hasProp(data, 'pressure') && (
-              <p className="day__pressure">{data.pressure} hPa</p>
-            )}
-          </div>
-        )}
-      </details>
-    </section>
+          {hasProp(data, 'dew_point') && (
+            <p className="day__dew-point">
+              {round(data.dew_point, 1)}&deg; dew point
+            </p>
+          )}
+          {hasProp(data, 'humidity') && (
+            <p className="day__humidity">{data.humidity}% humidity</p>
+          )}
+          {hasProp(data, 'pressure') && (
+            <p className="day__pressure">{data.pressure} hPa</p>
+          )}
+        </div>
+      )}
+    </details>
   );
 };
 
