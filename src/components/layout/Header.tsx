699e57e1c1e410e1d89df391c33c95e9b5af2688
Luka Bacic <luka.bacic278@gmail.com>
2021-03-07 19:01:11 +1100

    Add a link to saved location page

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,11 +1,14 @@
 import React, { useContext } from 'react';
 import { GlobalStateContext } from 'context';
 import Nav from './Nav';
+import { Link } from 'gatsby';
 
 const Header = () => {
   return (
     <header className="header">
-      Page name
+      <Link className="header__link" to="/saved-locations/">
+        Saved locations [current location]
+      </Link>
       <Nav />
     </header>
   );
---
+++ src/pages/saved-locations.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SavedLocations = () => {
+  return <div>asdf</div>;
+};
+
+export default SavedLocations;
